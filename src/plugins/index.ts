48b026d19529482b5843e5084b040442af1c10b0
stbearangel <stbearangel@sina.com>
2025-04-14 22:19:56 +0800

    系统引入状态依赖pinia以及状态持久化依赖pinia-plugin-persistedstate

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -10,9 +10,14 @@ import router from '../router'
 
 // Types
 import type { App } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 export function registerPlugins (app: App) {
+  const pinia = createPinia()
+  pinia.use(piniaPluginPersistedstate)
   app
     .use(vuetify)
     .use(router)
+    .use(pinia)
 }
