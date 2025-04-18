a120d0ddcebe8108f309893773021c21e8c1bd14
stbearangel <stbearangel@sina.com>
2025-04-18 22:35:26 +0800

    添加inversify做ioc容器。并研发相关工具类 添加vitest单元测试依赖

---
+++ src/stores/loginContextStore.ts
@@ -0,0 +1,27 @@
+import { defineStore } from "pinia";
+import { LoginContext } from "@/models/loginContext";
+
+export const loginContextStore = defineStore("loginContent", {
+  state: (): { loginContext: LoginContext | null } => ({
+    loginContext: null
+  }),
+  persist: {
+    key: "loginContent",
+    storage: sessionStorage
+  },
+  actions: {
+    /**
+     * 设置登录上下文
+     * @param context 登录上下文对象 {@link LoginContext}
+     */
+    setLoginContext(context: LoginContext) {
+      this.loginContext = context;
+    },
+    /**
+     * 情况上下文
+     */
+    clearLoginContext() {
+      this.loginContext = null;
+    },
+  },
+});
